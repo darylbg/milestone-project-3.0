5542b114c057dbd3a27632f5d6f133eed598ff27
darylbg <44724776+darylbg@users.noreply.github.com>
2020-02-21 15:46:49 +0000

    update edittask.html, addtask.html, app.py, applyform.html

--- app.py
+++ app.py
@@ -30,6 +30,32 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    return render_template('edittask.html', task=the_task)
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'camp_title':request.form.get('camp_title'),
+        'camp_state':request.form.get('camp_state'),
+        'camp_address': request.form.get('camp_address'),
+        'camp_availability': request.form.get('camp_availability'),
+        'camp_description':request.form.get('camp_description')
+    })
+    return redirect(url_for('get_tasks'))
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
+@app.route('/apply_form')
+def apply_form():
+    return render_template('applyform.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
